e1e37a515ab01b347712d3cb2b4b5ce1ac474328
fouedjerbi <114487005+fouedjerbi@users.noreply.github.com>
2022-11-29 16:23:02 +0100

    Add files via upload

---
+++ mission3.js
@@ -0,0 +1,10 @@
+function score () {
+  var juste, faute=0
+  if (document.getElementById(nbCheck1).checked==true && document.getElementById(nbCheck1).checked==false)
+   {
+    juste=juste+1;
+  }
+  else{
+    fautes= fautes+1;
+  }
+}
